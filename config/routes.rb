84b7a3106674552453bbe0b42d2ce73377ef4b45
Flavio-LP <flavio5070@outlook.com>
2026-05-04 21:28:53 -0300

    Feat: Was updated docker configuration and included Sidekiq Gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
+require "sidekiq/web"
+
 Rails.application.routes.draw do
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  mount Sidekiq::Web => "/sidekiq"
+
   namespace :api do
     namespace :v1 do
       get "climatempo/15dias/forquilhinha", to: "forecast#climatempo_15d"
